e6ada30e789cea8855d50fb278f7cc36cc81e68f
Benjamin Krueger <konto@blk.pm>
2022-02-14 22:12:33 +0100

    Basic functionality

--- Game.py
+++ Game.py
@@ -1,4 +1,33 @@
-import Room
+import json
+from Room import Room
+
+def lost():
+    print("You're feeling a little lost.")
+    print("You could *cry* for help.")
+
+def help():
+    print("Following commands are available:")
+    print("cry, help, quit")
+
+f = open("data.json")
+data = json.load(f)
+
+for i in data["rooms"]:
+    print(i["room_name"])
+
 myroom = Room()
 myroom.name = "Start"
-print (myroom.name)
+print(myroom.name)
+
+while True:
+    cmd = input("> ")
+    if (cmd == "help"):
+        help()
+    elif (cmd == "cry"):
+        help()
+    elif (cmd == "quit"):
+        break
+    else:
+        lost()
+
+f.close()
