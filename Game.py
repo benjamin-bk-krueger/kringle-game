33058a03d27d405e23c9bc662c9cee19cf477ea2
Benjamin Krueger <konto@blk.pm>
2022-02-16 22:01:51 +0100

    rework

--- Game.py
+++ Game.py
@@ -17,7 +17,7 @@ objectives = dict()     # contains all available objectives
 junctions = dict()      # contains all junctions between the rooms
 
 volcab = []             # contains autocomplete values
-default_actions = ['help','cry','exit','inspect', 'look', 'meditate','talk','quit','walk'] # default actions
+default_actions = ['help','cry','beam','exit','inspect','look','meditate','scrutinize','talk','phone','quit','walk'] # default actions
 
 # ANSII images have been created with the help of https://manytools.org/hacker-tools/convert-image-to-ansi-art/
 # size formats: 80, 160 characters (s, m format)
@@ -25,9 +25,15 @@ default_actions = ['help','cry','exit','inspect', 'look', 'meditate','talk','qui
 # all the actions the player can perform
 # only triggered once automatically when the player arrives (starts the program) - no command assigned
 def arrive():
-    print ("You are arriving at a strange location.")
+    print ("You are arriving at a strange and unknown location.")
     print ("You are feeling a little dizzy.")
     rooms.get(location).visited = True
+
+# get some about information - "scrutinize" command assigned
+def scrutinize():
+    print("A magical voice whispers into your ear:")
+    print("\"This game has been created by Ben Krueger\"")
+    print("You wonder what this may mean? A game? Strange thought")
 
 # triggered automatically when the player enters a wrong command - no command assigned
 def lost():
@@ -38,6 +44,7 @@ def help():
     print("A magical voice whispers into your ear: \"Following commands are available\"")
     print(default_actions)
     print("Then it whispers: \"TAB is your friend\"")
+    print("You wonder what this TAB may be? A creature? A scroll?")
 
 # have a detailed look what kind of objects a room contains - "inspect" command assigned
 def inspect():
@@ -64,6 +71,7 @@ def inspect():
 def meditate():
     print("A quest to save Santa has brought you to this place.")
     print("You think about all those creatures here could help you.")
+    print("It may also be a good idea to visit other places.")
 
 # have a quick look at this place - "look" command assigned
 def look():
@@ -71,7 +79,42 @@ def look():
     display_image(rooms.get(location).image)
     print(rooms.get(location).description)
 
-# talk to other creatues - "talk" command assigned
+# phone other creatures you already have discovered - "phone" command assigned
+def phone():
+    # necessary for creature name auto-completion
+    global volcab
+    new_volcab = []
+
+    print("You put your hand into your right pocket and grab a magical device.")
+    print("It has a display where you can see the names of all the creatures you had contact with.")
+    print("You guess you can open some channel to a creature by tapping its name.")
+
+    # assign all visited creatures to the auto-completion list
+    counter = 0
+    for id in objectives:
+        if (objectives[id].visited):
+            counter = counter + 1
+            new_volcab.append(objectives[id].name)
+            print("  Entry: " + objectives[id].name)
+
+    if (counter > 0):
+        set_custom_complete(new_volcab)
+        print("")
+        talk = input("I want to talk to > ")
+        for id in objectives:
+            if (objectives[id].visited):
+                if (objectives[id].name == talk):
+                    print("You are talking to " + objectives[id].name)
+                    display_image(objectives[id].image)
+                    break
+                else:
+                    print("You decide you don't want to talk right now.")
+                    break
+    else:
+        print("After a moment you realize you have not met anyone yet.")
+    set_default_complete()
+    
+# talk to other creatures - "talk" command assigned
 def talk():
     # necessary for creature name auto-completion
     global volcab
@@ -83,6 +126,7 @@ def talk():
         if (objectives[id].location == location):
             counter = counter + 1
             new_volcab.append(objectives[id].name)
+            print("  Entry: " + objectives[id].name)
 
     if (counter > 0):
         set_custom_complete(new_volcab)
@@ -94,10 +138,52 @@ def talk():
                     print("You are talking to " + objectives[id].name)
                     display_image(objectives[id].image)
                     objectives[id].visited = True
+                    break
                 else:
                     print("You decide you don't want to talk right now.")
+                    break
     else:
-        "After a moment you realize no one is in this room."
+        print("After a moment you realize no one is in this room.")
+    set_default_complete()
+
+# beam to places you already have discovered - "beam" command assigned
+def beam():
+    # necessary for room name auto-completion and new room selection
+    global volcab
+    global location
+    new_volcab = []
+
+    print("You put your hand into your left pocket and grab a magical device.")
+    print("It has a display where you can see the names of all the places you have visited.")
+    print("You guess you can travel there by tapping its name.")
+
+    # assign all visited rooms to the auto-completion list
+    counter = 0
+    new_location = 1
+    for id in rooms:
+        if (rooms[id].visited):
+            counter = counter + 1
+            new_volcab.append(rooms[id].name)
+            print("  Entry: " + rooms[id].name)
+
+    if (counter > 0):
+        set_custom_complete(new_volcab)
+        print("")
+        walk = input("I want to go to > ")
+        for id in rooms:
+            if (rooms[id].visited):
+                if (rooms[id].name == walk):
+                    print("You are going to " + rooms[id].name)
+                    new_location = id
+                    break
+                else:
+                    new_location = location
+                    print("You decide to stay where you currently are.")
+                    break
+    else:
+        new_location = location
+        print("After a moment you realize you have not been anywhere yet.")
+    location = new_location
     set_default_complete()
 
 # walk to other places - "walk" command assigned
@@ -114,6 +200,7 @@ def walk():
         if (junctions[id].location == location):
             counter = counter + 1
             new_volcab.append(rooms.get(junctions[id].destination).name)
+            print("  Entry: " + rooms.get(junctions[id].destination).name)
 
     if (counter > 0):
         set_custom_complete(new_volcab)
@@ -125,9 +212,11 @@ def walk():
                     print("You are going to " + rooms.get(junctions[id].destination).name)
                     new_location = junctions[id].destination
                     rooms.get(junctions[id].destination).visited = True
+                    break
                 else:
                     new_location = location
                     print("You decide to stay where you currently are.")
+                    break
     else:
         new_location = location
         "After a moment you realize there is no way out of this room."
@@ -195,6 +284,8 @@ def query_user():
         help()
     elif (cmd == "cry"):
         help()
+    elif (cmd == "beam"):
+        beam()
     elif (cmd == "exit"):
         cont = 0
     elif (cmd == "inspect"):
@@ -203,8 +294,12 @@ def query_user():
         look()
     elif (cmd == "meditate"):
         meditate()
+    elif (cmd == "scrutinize"):
+        scrutinize()
     elif (cmd == "talk"):
         talk()
+    elif (cmd == "phone"):
+        phone()
     elif (cmd == "quit"):
         cont = 0
     elif (cmd == "walk"):
