2395c5be94c0c210d076031ea87b3f3e1764e33a
Benjamin Krueger <konto@blk.pm>
2022-04-18 21:13:17 +0200

    Added image support

--- db/init.sql
+++ db/init.sql
@@ -2,8 +2,8 @@ CREATE TABLE creator (
     creator_id SERIAL PRIMARY KEY,
     creator_name VARCHAR ( 100 ) UNIQUE NOT NULL,
     creator_pass VARCHAR ( 256 ),
-    creator_hash VARCHAR ( 256 ),
-    creator_role VARCHAR ( 10 )
+    creator_role VARCHAR ( 10 ),
+    creator_img VARCHAR ( 384 )
 );
 
 CREATE TABLE world (
@@ -11,14 +11,16 @@ CREATE TABLE world (
     creator_id INT REFERENCES creator ( creator_id ),
     world_name VARCHAR ( 100 ) UNIQUE NOT NULL,
     world_desc VARCHAR ( 1024 ),
-    world_url VARCHAR ( 256 )
+    world_url VARCHAR ( 256 ),
+    world_img VARCHAR ( 384 )
 );
 
 CREATE TABLE room (
     room_id SERIAL PRIMARY KEY,
     world_id INT REFERENCES world ( world_id ),
     room_name VARCHAR ( 100 ),
-    room_desc VARCHAR ( 1024 ) 
+    room_desc VARCHAR ( 1024 ),
+    room_img VARCHAR ( 384) 
 );
 
 CREATE UNIQUE INDEX idx_room_name
@@ -29,7 +31,8 @@ CREATE TABLE item (
     room_id INT REFERENCES room ( room_id ),
     world_id INT REFERENCES world ( world_id ),
     item_name VARCHAR ( 100 ),
-    item_desc VARCHAR ( 1024 )
+    item_desc VARCHAR ( 1024 ),
+    item_img VARCHAR ( 384 )
 );
 
 CREATE UNIQUE INDEX idx_item_name
@@ -44,7 +47,8 @@ CREATE TABLE objective (
     difficulty INT,
     objective_url VARCHAR ( 256 ),
     supported_by VARCHAR ( 100 ),
-    requires VARCHAR ( 100 )
+    requires VARCHAR ( 100 ),
+    objective_img VARCHAR ( 384 )
 );
 
 CREATE UNIQUE INDEX idx_objective_name
@@ -55,7 +59,8 @@ CREATE TABLE person (
     room_id INT REFERENCES room ( room_id ),
     world_id INT REFERENCES world ( world_id),
     person_name VARCHAR ( 100 ),
-    person_desc VARCHAR ( 1024 )
+    person_desc VARCHAR ( 1024 ),
+    person_img VARCHAR ( 384 )
 );
 
 CREATE UNIQUE INDEX idx_person_name
