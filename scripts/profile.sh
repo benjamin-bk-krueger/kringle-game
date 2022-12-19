98195ff8e3d8f63b1fbabc9652748c2807969a27
Benjamin Krueger <konto@blk8.de>
2022-12-19 10:54:48 +0100

    Fixed old and broken code

---
+++ scripts/profile.sh
@@ -0,0 +1,10 @@
+# Python venv
+export PATH=~/venv/bin:$PATH
+
+# Default credentials, need to be changed on production stage
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_USER=kringle
+export POSTGRES_PW=kringle
+export POSTGRES_DB=kringle
+export S3_FOLDER=http://localhost:9000/kringle-public
